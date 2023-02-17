6603c7d27424c84147175393ec77fa8b6cb691c1
MaríaJosé00 <mjquishpeg@uce.edu.ec>
2023-02-17 06:23:31 -0500

    1

--- src/main/java/com/programacion/distribuida/rest/AuthorRest.java
+++ src/main/java/com/programacion/distribuida/rest/AuthorRest.java
@@ -2,6 +2,11 @@ package com.programacion.distribuida.rest;
 
 import com.programacion.distribuida.db.Authors;
 import com.programacion.distribuida.servicios.AuthorRepository;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -17,6 +22,10 @@ public class AuthorRest {
 
     @GET
     @Path("/{id}")
+    @Operation(summary = "Buscar",description = "Busca segun el id")
+    @APIResponse(responseCode = "200",description = "Correcto",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON,
+                    schema = @Schema(implementation = Authors.class)))
     public Authors findById(@PathParam("id") Long id) {
         return repository.findById(id);
     }
@@ -24,6 +33,10 @@ public class AuthorRest {
     @Inject AuthorRepository repository;
 
     @GET
+    @Operation(summary = "Buscar",description = "Lista todos")
+    @APIResponse(responseCode = "200",description = "Correcto",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON,
+                    schema = @Schema(implementation = Authors.class)))
     public List<Authors> findAll() {
         return repository
                 .findAll()
@@ -31,12 +44,20 @@ public class AuthorRest {
     }
 
     @POST
+    @Operation(summary = "Insertar",description = "Crea un nuevo registro")
+    @APIResponse(responseCode = "200",description = "Correcto",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON,
+                    schema = @Schema(implementation = Authors.class)))
     public void insert(Authors obj) {
         repository.persist(obj);
     }
 
     @PUT
     @Path("/{id}")
+    @Operation(summary = "Actualizar",description = "Modifica un elemento segun el id")
+    @APIResponse(responseCode = "200",description = "Correcto",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON,
+                    schema = @Schema(implementation = Authors.class)))
     public void update(Authors obj, @PathParam("id") Long id) {
 
         var author = repository.findById(id);
@@ -47,6 +68,10 @@ public class AuthorRest {
 
     @DELETE
     @Path("/{id}")
+    @Operation(summary = "Eliminar",description = "Elimina elementos por el id")
+    @APIResponse(responseCode = "200",description = "Correcto",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON,
+                    schema = @Schema(implementation = Authors.class)))
     public void delete( @PathParam("id") Long id ) {
         repository.deleteById(id);
     }
